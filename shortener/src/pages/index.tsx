6c753c9a5c070f024b99ae44270b8328026f71bc
myk19977 <myk19977@gmail.com>
2023-03-26 14:23:22 +0900

    スタイル調整

--- shortener/src/pages/index.tsx
+++ shortener/src/pages/index.tsx
@@ -40,24 +40,49 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>短縮URL生成サービス</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter URL"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <button type="submit">短縮URLを生成</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="50"
+            height="50"
+          >
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M4 4h16v16H4z"></path>
+              <path d="M12 7l5 5-5 5"></path>
+              <path d="M12 12H7"></path>
+            </g>
+          </svg>
+        </div>
+        <h1 className={styles.title}>短縮URL生成サービス</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={styles.urlInput}
+            type="text"
+            placeholder="Enter URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button className={styles.generateButton} type="submit">
+            短縮URLを生成
+          </button>
+        </form>
 
-      {error && <p className="error">{error}</p>}
-      {shortUrl && (
-        <p>
-          短縮URLはこちら: <a href={shortUrl}>{shortUrl}</a>
-        </p>
-      )}
+        {error && <p className={styles.error}>{error}</p>}
+        {shortUrl && (
+          <p className={styles.shortUrlInfo}>
+            短縮URLはこちら: <a href={shortUrl}>{shortUrl}</a>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
